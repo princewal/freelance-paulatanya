af1901d4c1f917a7121bb3aefe0fa3ba27daa1ee
W@l33D <princewal@hotmail.com>
2025-01-07 16:36:51 -0700

    added a carousel for one of the sections and wrapped up initial site.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-import './styles/scss/global.scss'
+import "./styles/scss/global.scss";
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  delay: 5000,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
