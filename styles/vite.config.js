4fa62383e6debcbce51e2be7d97011adf4b2d6c1
W@l33D <princewal@hotmail.com>
2025-04-03 04:22:53 -0600

    update vite config file

--- styles/vite.config.js
+++ styles/vite.config.js
@@ -1,21 +1,29 @@
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from "vite";
+import path from "path";
 
-
-export default defineConfig ({   
-    css: {
-        preprocessorOptions: {
-          scss: {
-            additionalData: `@import "${path.resolve(__dirname, 'styles/scss/global.scss')}";`
-          }
-        }
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${path.resolve(
+          __dirname,
+          "styles/scss/global.scss"
+        )}";`,
       },
-      build: {
-        rollupOptions: {
-          input: 'styles/scss/global.scss',
-          output: {
-            file: 'styles/styles.css'
-          }
-        }
-      }
-})
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        /* testimonial: path.resolve(__dirname, 'testimonial.html'), */
+        "south-sudan": path.resolve(__dirname, "south-sudan.html"),
+      },
+      lib: {
+        entry: path.resolve(__dirname, "styles/scss/global.scss"),
+        name: "styles", // You can name the library here (just for better identification)
+        fileName: "styles/styles", // The final output file will be styles/styles.css
+      },
+    },
+  },
+});
