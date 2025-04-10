0f1e61ab872dc8782c68c02c9d8fd28c3826e3c2
W@l33D <princewal@hotmail.com>
2025-04-10 15:01:06 -0600

    add more responsive css to the pages and added menu button to all pages

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 import "./styles/scss/global.scss";
 
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-  effect: "fade",
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+document.addEventListener("DOMContentLoaded", () => {
+  try {
+    const swiper = new Swiper(".swiper", {
+      loop: true,
+      /* autoplay: {
+        delay: 5000,
+      }, */
+      effect: "fade",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  } catch (error) {
+    console.error("Error initializing the application:", error);
+  }
 
-const menuButton = document.querySelector(".menu-button");
-const navbar = document.querySelector(".navbar");
-menuButton.addEventListener("click", () => {
-  navbar.classList.toggle("opened");
-  this.setAttribute("aria-expanded", navbar.classList.contains("opened"));
+  const menuButton = document.querySelector(".menu-button");
+  const navbar = document.querySelector(".navbar");
+  menuButton.addEventListener("click", (e) => {
+    console.log("clicked", this);
+    navbar.classList.toggle("opened");
+    menuButton.setAttribute(
+      "aria-expanded",
+      navbar.classList.contains("opened")
+    );
+    document.body.classList.toggle("no-scroll");
+  });
 });
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   css: {
     preprocessorOptions: {
       scss: {},
